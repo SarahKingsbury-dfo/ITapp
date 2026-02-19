c45544264bb3be8fcd19a9915d9eb2b0e3f22dca
SarahKingsbury-dfo <sarah.kingsbury@dfo-mpo.gc.ca>
2026-02-19 13:50:34 -0400

    Incidental Reports 2025
    Updated incidental data reports to incorporate all the reports from 2025.

--- prepare_data.R
+++ prepare_data.R
@@ -38,8 +38,6 @@ PEI <- readRDS("spatialdata/PEI.rds")
 
 species <- read.csv("commonnames.csv")
 
-
-
 # Load and clean up incidental data ---------------------------------------
 
 searcharea <- c(NS$geometry,NB$geometry,PEI$geometry) %>% 
@@ -65,20 +63,23 @@ incidental_occ <- occ(query=species$Scientific_Name,
   ))
 
 incidental_occ<-incidental_occ%>%
+  filter(!grepl("BOLD", name))%>% #remove any columns form the BOLD database
   mutate(Species=case_when(name %in% c("Aequipecten irradians (Lamarck, 1819)","Argopecten irradians (Lamarck, 1819)", "Argopecten irradians", "Argopecten irradians irradians", "Aequipecten irradians sablensis A.H.Clarke, 1965" ,"Argopecten irradians sablensis (A.H.Clarke, 1965)") ~ "Argopecten_irradians",
                            name %in% c("Ascidiella aspersa (Müller, 1776)", "Ascidiella aspersa (M?ller, 1776)","Ascidiella aspersa" ) ~ "Ascidiella_aspersa",
                            name %in% c("BOLD:AAA7687","BOLD:ACL8382","Carcinus maenas (Linnaeus, 1758)", "Carcinus maenas") ~ "Carcinus_maenas",
                            name %in% c("Botrylloides violaceus Oka, 1927", "Botrylloides violaceus") ~ "Botrylloides_violaceus",
                            name %in% c("Botryllus schlosseri (Pallas, 1766)", "Botryllus schlosseri") ~ "Botryllus_schlosseri",
                            name %in% c("Caprella mutica Schurin, 1935","BOLD:AAE7686", "Caprella mutica") ~ "Caprella_mutica",
-                           name %in% c("Carcinus maenas (Linnaeus, 1758)")~"Carcinus maenas",
+                           name %in% c("Carcinus maenas (Linnaeus, 1758)", "Carcinus maenas")~"Carcinus maenas",
                            name %in% c("Ciona intestinalis (Linnaeus, 1767)","Ciona intestinalis tenella (Stimpson, 1852)","Ciona tenella (Stimpson, 1852)", "Ciona intestinalis" ) ~ "Ciona_intestinalis",
-                           name %in% c("Codium fragile (Suringar) Hariot","Codium fragile subsp. fragile","Codium fragile subsp. tomentosoides (Goor) P.C.Silva","Codium fragile tomentosoides", "Codium fragile", "Codium fragile (Suringar) Har." ) ~ "Codium_fragile",
+                           name %in% c("Codium fragile (Suringar) Hariot","Codium fragile subsp. fragile","Codium fragile subsp. tomentosoides (Goor) P.C.Silva","Codium fragile tomentosoides", "Codium fragile", "Codium fragile (Suringar) Har." ,"Codium fragile var. fragile") ~ "Codium_fragile",
+                           name %in% c("Diadumene lineata (Verrill, 1869)", "Diadumene lineata")~"Diadumene_lineata",
                            name %in% c("Didemnum vexillum Kott, 2002", "Didemnum vexillum") ~ "Didemnum_vexillum",
                            name %in% c("Diplosoma listerianum (Milne Edwards, 1841)", "Diplosoma listerianum") ~ "Diplosoma_listerianum",
+                           name %in% c("Fucus serratus L.", "Fucus serratus")~"Fucus_serratus",
                            name %in% c("Hemigrapsus sanguineus (De Haan, 1835)","Hemigrapsus sanguineus (de Haan, 1835)", "Hemigrapsus sanguineus") ~ "Hemigrapsus_sanguineus",
                            name %in% c("Juxtacribrilina mutabilis (Ito, Onishi & Dick, 2015)","Juxtacribrilina mutabilis")~"Juxtacribrilina_mutabilis",
-                           name %in% c("Membranipora membranacea (Linnaeus, 1767)", "Flustra membranacea Linnaeus, 1767", "Membranipora membranacea" ) ~ "Membranipora_membranacea",
+                           name %in% c("Membranipora membranacea (Linnaeus, 1767)", "Flustra membranacea Linnaeus, 1767", "Membranipora membranacea") ~ "Membranipora_membranacea",
                            name %in% c("Oncorhynchus mykiss (Walbaum, 1792)", "Oncorhynchus mykiss") ~ "Oncorhynchus_mykiss",
                            name %in% c("Ostrea edulis (Linnaeus, 1767)","Ostrea edulis Linnaeus, 1758","Ostrea edulis") ~ "Ostrea_edulis",
                            name %in% c("Sargassum muticum (Yendo) Fensholt", "Sargassum muticum")~"Sargassum_muticum",
@@ -195,12 +196,36 @@ gulf_tunicate_incidental_2024<-readxl::read_excel("recentdata/Gulf 2024 AIS Data
   mutate(prov="Gulf Science Data contact Renee.Bernier@dfo-mpo.gc.ca") %>% 
   st_cast('POINT')
 
+gulf_tunicate_incidental_2025<-readxl::read_excel("recentdata/Gulf 2025 AIS Data_Science Gulf_Feb2026.xlsx",sheet=2,col_types =  "text") %>% 
+  st_as_sf(coords=c('Longitude','Latitude'),crs=4326) %>% 
+  dplyr::rename(StnLocation=`Location Name`,
+                "Botryllus_schlosseri"="B schlosseri",
+                "Botrylloides_violaceus"="B violaceus",
+                "Ciona_intestinalis"="C intestinalis",
+                "Styela_clava"="S clava",
+                "Membranipora_membranacea"="M membranacea",
+                "Carcinus_maenas"="C maenas",
+                "Codium_fragile"="C fragile",
+                "Juxtacribrilina_mutabilis"="J mutabilis",
+                "Fucus_serratus"="F serratus",
+                "Caprella_mutica"="C mutica") %>% 
+  dplyr::select(-Province,-Comments) %>% 
+  gather(key = "Species", value = "Presence",-StnLocation,-Year,-geometry) %>% 
+  group_by(Species,StnLocation,Year) %>% 
+  summarize(Presence = if_else(all(is.na(Presence)),
+                               FALSE,
+                               any(Presence>0,na.rm = TRUE))) %>% 
+  ungroup() %>% 
+  filter(Presence) %>% 
+  mutate(prov="Gulf Science Data contact Renee.Bernier@dfo-mpo.gc.ca") %>% 
+  st_cast('POINT')
+
 mar_incidental <- read.csv("recentdata/Incidental_AIS_Reports_MAR.csv")%>%
+  filter(!Lat == 'NA')%>%
   sf::st_as_sf(coords=c('Lon','Lat'),crs=4326) %>% 
   filter(Picture_Confirmation==1) %>% 
   mutate(Presence=as.logical(Picture_Confirmation),
-         prov = paste("Maritimes Incidental Data:", "Contact Sarah.Kingsbury@dfo-mpo.gc.ca"),
-         Year = 2021,
+         prov = paste("Maritimes Incidental Data:", "Contact Sarah.Kingsbury@dfo-mpo.gc.ca")
          ) %>% 
   dplyr::select(Species,StnLocation,Year,prov)%>%
   filter(Species %in% species$R_Name) #only keep species of relevance to I&T transfers
@@ -217,6 +242,8 @@ incidental_sites <- rbind(
   gulf_tunicate_incidental_2023%>% 
     dplyr::select(StnLocation),
   gulf_tunicate_incidental_2024%>% 
+    dplyr::select(StnLocation),
+  gulf_tunicate_incidental_2025%>% 
     dplyr::select(StnLocation),
   mar_incidental%>%
     dplyr::select(StnLocation)
@@ -249,6 +276,9 @@ incidental <-  dplyr::bind_rows(
     dplyr::mutate(across(.fns = as.character))%>%
     as.data.table(),
   gulf_tunicate_incidental_2024 %>%
+    dplyr::mutate(across(.fns = as.character))%>%
+    as.data.table(),
+  gulf_tunicate_incidental_2025 %>%
     dplyr::mutate(across(.fns = as.character))%>%
     as.data.table(),
   mar_incidental%>%
